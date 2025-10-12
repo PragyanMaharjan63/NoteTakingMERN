5e2c526beec9cae962e416b10235ce65450afabd
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-10-12 18:58:24 +0545

    few changes

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../components/home";
 import Login from "../components/login";
 import Signin from "../components/signin";
 import Navbar from "../components/navbar";
+import { UseBackend } from "../contexts/context";
 
 export default function Router() {
+  const { loggedIn } = UseBackend();
   return (
     <>
       <div className="w-full md:w-[80vw] fixed z-9">
@@ -12,7 +14,10 @@ export default function Router() {
       </div>
       <div className="absolute grid place-items-center w-full h-full">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={loggedIn ? <Home /> : <Navigate to="/signin" replace />}
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />
 
