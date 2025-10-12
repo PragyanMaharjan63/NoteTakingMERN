10e7ccc5ed1f37046c0331847c5ff21fadef34fd
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-10-12 19:48:29 +0545

    managing cors again2

--- backend/routes/notesRouter.js
+++ backend/routes/notesRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { authMiddleware } from "../controllers/authControllers.js";
 import {
   deleteNotes,
@@ -8,7 +9,7 @@ import {
 } from "../controllers/notesController.js";
 
 const noteRouter = express.Router();
-
+noteRouter.use(cors(crossOptions));
 noteRouter.get("/getnotes", authMiddleware, getNotes);
 noteRouter.post("/setnotes", authMiddleware, setNotss);
 noteRouter.post("/deletenote/:id", authMiddleware, deleteNotes);
