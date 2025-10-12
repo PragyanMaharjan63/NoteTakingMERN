f826a15441202c56bc05e1ba74172984e97b31e8
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-10-12 21:18:32 +0545

    cors is shit fr

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,8 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://note-taking-mern-1w1w.vercel.app",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
